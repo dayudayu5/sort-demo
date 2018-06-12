88279548c8c7ac3074525c4a3da365550e38f7d5
dayudayu5 <35829901+dayudayu5@users.noreply.github.com>
2018-06-12 14:46:15 +0800

    insertSort

--- src/com/yu/straightInsertionSort/StraightInsertionSort.java
+++ src/com/yu/straightInsertionSort/StraightInsertionSort.java
@@ -11,6 +11,41 @@ public class StraightInsertionSort {
      * @param l 数组长度
      */
     public static void insertSort(int[] a, int l) {
+        int i,j,k;
+        for (i = 0; i < l ; i++) {
+            for (j = i - 1; j > 0 ; j--) {
+                if (a[j] < a[i])
+                    break;//找到合适位置
+            }
+            if (j != i - 1) {
+                //位置后面元素后移
+                int temp = a[i];
+                for (k = i - 1; k > j; k--) {
+                    a[k + 1] = a[k];
+                }
+                a[k + 1] = temp;
+            }
+        }
+
+    }
+
+    /**
+     * 测试
+     * @param args
+     */
+    public static void main(String[] args) {
+        int[] a = {1,88,64,45,78,98,22,34};
+        int l = a.length;
+        System.out.println("------排序之前------");
+        for (int tem : a) {
+            System.out.print(tem + " ");
+        }
+        System.out.println();
+        System.out.println("------排序之后------");
+        insertSort(a,l);
+        for (int tem : a) {
+            System.out.print(tem + " ");
+        }
 
     }
 }
